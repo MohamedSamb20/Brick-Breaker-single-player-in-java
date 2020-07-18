1f1e6db238a95826ff1b8780a6ede123f9e282dc
Mohamed Samb <46804837+MohamedSamb20@users.noreply.github.com>
2020-07-18 01:19:28 -0700

    Create Brick.java

---
+++ Brick.java
@@ -0,0 +1,15 @@
+public class Brick
+{
+    int x;
+    int y;
+    int h;
+    int w;
+    
+    public Brick(int x, int y, int h, int w)
+    {
+        this.x = x;
+        this.y = y;
+        this.h = h;
+        this.w = w;
+    }
+}
